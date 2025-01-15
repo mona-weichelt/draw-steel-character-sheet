6d650f91416b39d6f4fe641f1609f8d11f4bfb73
Mona Weichelt <morgana.weichelt@gmail.com>
2025-01-15 23:55:56 +0100

    aded debug stamina menu

--- app/sheet/_layout.tsx
+++ app/sheet/_layout.tsx
@@ -3,12 +3,35 @@ import StaminaBar from "@/components/stamina/StaminaBar";
 import StaminaDisplay from "@/components/stamina/StaminaDisplay";
 import { useHeroContext } from "@/hooks/useHeroContext";
 import { Slot } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 export default function SheetLayout() {
   const { state, dispatch } = useHeroContext();
   return (
     <>
+      <View className="flex flex-row gap-2 p-2">
+        <View className="flex-1">
+          <Pressable
+            className="h-12 items-center justify-center bg-red-600 rounded-lg"
+            onPress={() =>
+              dispatch({
+                type: "Take Damage",
+                payload: 65,
+              })
+            }
+          >
+            <Text className="text-white font-bold">Take Damage</Text>
+          </Pressable>
+        </View>
+        <View className="flex-1">
+          <Pressable
+            className="h-12 items-center justify-center bg-green-600 rounded-lg"
+            onPress={() => dispatch({ type: "Use Recovery" })}
+          >
+            <Text className="text-white font-bold">Use Recovery</Text>
+          </Pressable>
+        </View>
+      </View>
       <View className="p-2 border-b bg-gray-800 flex flex-col gap-2">
         <View className="flex flex-row flex-wrap justify-between">
           <StaticDisplay
