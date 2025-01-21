f0e5b9c251c4a87df111bd19b1a6ad22233a06f8
Mona Weichelt <morgana.weichelt@gmail.com>
2025-01-21 16:40:46 +0100

    improved influence display

--- app/sheet/(tabs)/skills.tsx
+++ app/sheet/(tabs)/skills.tsx
@@ -13,6 +13,9 @@ export default function HeroSkills() {
     state: { wealth, renown },
   } = useHeroContext();
 
+  const wealthLabel =
+    "Mundane clothing, gear, armor, implements, and weapons; meals or drinks at a common tavern; stay at a common inn; passage on a boat";
+
   return (
     <ScrollView
       className="flex flex-1 px-2 py-4 bg-gray-800"
@@ -21,10 +24,17 @@ export default function HeroSkills() {
       <Characteristics />
       <SkillDisplay />
       <TestResultTable />
-      <MenuEntry header="Influence" bodyClassName="pt-4">
-        <View className="flex-row justify-evenly">
+      <MenuEntry header="Influence" bodyClassName="pt-4 gap-4">
+        <View className="flex-row items-start gap-2">
           <StaticDisplay value={wealth} labelBottom="wealth" />
+          <Text className="text-white">{wealthLabel}</Text>
+        </View>
+        <View className="h-[1px] border-t border-gray-500 w-1/4 self-center border-dotted" />
+        <View className="flex-row items-start gap-2">
           <StaticDisplay value={renown} labelBottom="renown" />
+          <Text className="text-white">
+            Watch captain in a large city, high priest, viscount
+          </Text>
         </View>
       </MenuEntry>
       <LanguageDisplay />
--- components/data/StaticDisplay.tsx
+++ components/data/StaticDisplay.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from "react";
-import { Text, Pressable } from "react-native";
+import { Text, Pressable, View } from "react-native";
 
 const StaticDisplay = ({
   value,
   labelTop,
   labelBottom,
+  labelRight,
   className,
   onPress,
 }: {
   value: string | ReactNode;
   labelTop?: string | ReactNode;
   labelBottom?: string | ReactNode;
+  labelRight?: string | ReactNode;
   className?: string;
   onPress?: () => void;
 }) => {
@@ -18,18 +20,22 @@ const StaticDisplay = ({
     "uppercase text-white font-bold text-xs absolute rounded-lg min-w-20 w-full text-center";
   return (
     <Pressable
-      className={
-        "flex justify-center items-center w-[72px] h-12 border border-gray-500 bg-gray-700 rounded-lg mb-2 " +
-        className
-      }
+      className={"flex-row items-center " + className}
       onPress={onPress}
     >
-      {labelTop && <Text className={textStyle + " -top-2"}>{labelTop}</Text>}
-      <Text className="uppercase text-white w-full text-center font-bold text-2xl rounded-lg">
-        {value}
-      </Text>
-      {labelBottom && (
-        <Text className={textStyle + " -bottom-2"}>{labelBottom}</Text>
+      <View className="flex justify-center items-center w-[72px] h-12 border border-gray-500 bg-gray-700 rounded-lg">
+        {labelTop && <Text className={textStyle + " -top-2"}>{labelTop}</Text>}
+        <Text className="uppercase text-white w-full text-center font-bold text-2xl rounded-lg">
+          {value}
+        </Text>
+        {labelBottom && (
+          <Text className={textStyle + " -bottom-2"}>{labelBottom}</Text>
+        )}
+      </View>
+      {labelRight && (
+        <View className="flex-1 bg-gray-700 border border-l-0 border-gray-500 rounded-r-lg p-1 pr-2">
+          <Text className={"text-white text-wrap"}>{labelRight}</Text>
+        </View>
       )}
     </Pressable>
   );
