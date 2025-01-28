c340b1bda9775a2cff48b9e581d60c6d04a5389b
Mona Weichelt <morgana.weichelt@gmail.com>
2025-01-28 23:07:15 +0100

    added react native reanimated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,14 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["**/*.config.*", "**/*.eslintrc.js"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "warn",
+    },
+  },
 ];
