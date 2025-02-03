b0106d060a2da3efd608f69883ad1c2a1e5fc5f0
Mona Weichelt <morgana.weichelt@gmail.com>
2025-02-03 14:40:36 +0100

    added redirect to index

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,8 @@
-import { Link } from "expo-router";
+import { Redirect } from "expo-router";
 import React from "react";
-import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1">
-      <Link
-        className="w-full h-full text-center bg-blue-200"
-        href="/sheet/(tabs)/skills"
-      >
-        Go to sheet
-      </Link>
-    </View>
+    <Redirect href="/sheet/(tabs)/skills"/>
   );
 }
